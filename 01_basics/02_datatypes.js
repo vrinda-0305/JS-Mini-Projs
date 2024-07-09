ce8d5157ed2d710274726ca12bfb8c3701ea9487
vrinda-0305 <114207213+vrinda-0305@users.noreply.github.com>
2024-07-09 11:45:17 +0000

    added 02_datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+"use script";
+
+//number
+//bigint
+//string
+//boolean
+//null
+//undefined
+//symbol->unique
+
+console.log(typeof "Vrinda")
+console.log(typeof null)
+console.log(typeof undefined)
